4f481e294b6d3e69935da8cf3d44075adf8ac80b
Nick Miller <ngmiller@iastate.edu>
2014-11-15 23:22:37 -0600

    base containerService functionality and unit tests

--- app/js/docker/containerService.js
+++ app/js/docker/containerService.js
@@ -1,33 +1,171 @@
 /*
  * containerService
  * Builds and requests Docker container endpoints for a target host.
+ *
+ * Exposed API methods have the following parameters:
+ *  @param host - string identifying target host
+ *  @param query/data - query parameters or associated body data
+ *  @param headers - request headers
+ *  (optionally) @param id - string, container id
+ *
+ * Additional parameters are specified on the docstring.
+ * The method definition order follows the order of Docker's API docs as of v1.15
+ *
  */
 function containerService(Restangular) {
     'use strict';
 
     /*
      * dockerBase - private
-     * /{host}/d
-     *
-     * @param host - string identifying target host
+     * /{host}/d/containers
      */
     function dockerBase(host) {
-        return Restangular.all(host).all('d');
+        return Restangular
+            .one(host).one('d').one('containers');
     }
 
     /*
      * list()
-     * /{host}/d/containers/json
-     *
-     * @param host - string identifying target host
+     * GET /{host}/d/containers/json
      */
      function list(host, query, headers) {
         return dockerBase(host)
-            .one('containers').one('json').get(query, headers);
+            .one('json').get(query, headers);
+    }
+
+    /*
+     * create()
+     * POST /{host}/d/containers/create
+     */
+    function create(host, data, headers) {
+        return dockerBase(host)
+            .all('create').post(data, headers);
+    }
+
+    /*
+     * inspect()
+     * GET /{host}/d/containers/{id}/json
+     */
+    function inspect(host, id, query, headers) {
+        return dockerBase(host)
+            .one(id).one('json').get(query, headers);
+    }
+
+    /*
+     * top()
+     * GET /{host}/d/containers/{id}/top
+     */
+    function top(host, id, query, headers) {
+        return dockerBase(host)
+            .one(id).one('top').get(query, headers);
+    }
+
+    /*
+     * logs()
+     * GET /{host}/d/containers/{id}/logs
+     */
+    function logs(host, id, query, headers) {
+        return dockerBase(host)
+            .one(id).one('logs').get(query, headers);
+    }
+
+    /*
+     * changes()
+     * GET /{host}/d/containers/{id}/changes
+     */
+    function changes(host, id, query, headers) {
+        return dockerBase(host)
+            .one(id).one('changes').get(query, headers);
+    }
+
+    /*
+     * start()
+     * POST /{host}/d/containers/{id}/start
+     */
+    function start(host, id, query, headers) {
+        return dockerBase(host)
+            .one(id).one('start').post(query, headers);
+    }
+
+    /*
+     * stop()
+     * POST /{host}/d/containers/{id}/stop
+     */
+    function stop(host, id, query, headers) {
+        return dockerBase(host)
+            .one(id).one('stop').post(query, headers);
+    }
+
+    /*
+     * restart()
+     * POST /{host}/d/containers/{id}/restart
+     */
+    function restart(host, id, query, headers) {
+        return dockerBase(host)
+            .one(id).one('restart').post(query, headers);
+    }
+
+    /*
+     * kill()
+     * POST /{host}/d/containers/{id}/kill
+     */
+    function kill(host, id, query, headers) {
+        return dockerBase(host)
+            .one(id).one('kill').post(query, headers);
+    }
+
+    /*
+     * pause()
+     * POST /{host}/d/containers/{id}/pause
+     */
+    function pause(host, id, query, headers) {
+        return dockerBase(host)
+            .one(id).one('pause').post(query, headers);
+    }
+
+    /*
+     * unpause()
+     * POST /{host}/d/containers/{id}/unpause
+     */
+    function unpause(host, id, query, headers) {
+        return dockerBase(host)
+            .one(id).one('unpause').post(query, headers);
+    }
+
+    /*
+     * wait()
+     * POST /{host}/d/containers/{id}/wait
+     */
+    function wait(host, id, query, headers) {
+        return dockerBase(host)
+            .one(id).one('wait').post(query, headers);
+    }
+
+    /*
+     * remove()
+     * DELETE /{host}/d/containers/{id}
+     */
+    function remove(host, id, query, headers) {
+        return dockerBase(host)
+            .one(id).remove(query, headers);
     }
 
     return {
-        'list': list
+        // Currently a subset of all Docker container calls
+        'changes': changes,
+        'create': create,
+        'inspect': inspect,
+        'kill': kill,
+        'list': list,
+        'logs': logs,
+        'pause': pause,
+        'remove': remove,
+        'restart': restart,
+        'start': start,
+        'stop': stop,
+        'top': top,
+        'unpause': unpause,
+        'wait': wait
     };
 }
 
--- app/js/docker/tests/containerService.spec.js
+++ app/js/docker/tests/containerService.spec.js
@@ -1,37 +1,144 @@
 
-
 describe('containerService', function () {
-    var $httpBackend,
+    var http,
         containerService;
 
     beforeEach(function () {
         angular.mock.module('lighthouse.app');
 
-        angular.mock.inject(function (_$httpBackend_, _containerService_) {
-            $httpBackend = _$httpBackend_;
+        angular.mock.inject(function (_$httpBackend_, _containerService_, Restangular) {
+            http = _$httpBackend_;
             containerService = _containerService_;
+            // future proof API tests (note: this is RestangularProvider)
+            Restangular.setBaseUrl('/api/v0');
         });
     });
 
     it('should list containers', function () {
         // testing route is created as expected
-        $httpBackend.expect('GET',
+        http.expect('GET',
             '/api/v0/host/d/containers/json').respond('');
 
         containerService.list('host');
-        $httpBackend.flush();
+        http.flush();
     });
 
     it('should list containers with query parameters', function() {
-        $httpBackend.expect('GET',
+        http.expect('GET',
             '/api/v0/host/d/containers/json?all=true&size=true').respond('');
 
         containerService.list('host', {
             all: true,
             size: true
         });
-
-        $httpBackend.flush();
+        http.flush();
     });
+
+    it('should create a container', function () {
+        http.expect('POST',
+            '/api/v0/host/d/containers/create', {some: 'data'}).respond('');
+
+        containerService.create('host', {
+            some: 'data'
+        });
+        http.flush();
+    });
+
+    it('should inspect a container', function () {
+        http.expect('GET',
+            '/api/v0/host/d/containers/id/json').respond('');
+
+        containerService.inspect('host', 'id');
+        http.flush();
+    });
+
+    it('should list processes running', function () {
+        http.expect('GET',
+            '/api/v0/host/d/containers/id/top').respond('');
+
+        containerService.top('host', 'id');
+        http.flush();
+    });
+
+    it('should get container logs', function () {
+        http.expect('GET',
+            '/api/v0/host/d/containers/id/logs').respond('');
+
+        containerService.logs('host', 'id');
+        http.flush();
+    });
+
+    it('should get container changes', function () {
+        http.expect('GET',
+            '/api/v0/host/d/containers/id/changes').respond('');
+
+        containerService.changes('host', 'id');
+        http.flush();
+    });
+
+    it('should start a container', function () {
+        http.expect('POST',
+            '/api/v0/host/d/containers/id/start').respond('');
+
+        containerService.start('host', 'id');
+        http.flush();
+    });
+
+    it('should stop a container', function () {
+        http.expect('POST',
+            '/api/v0/host/d/containers/id/stop').respond('');
+
+        containerService.stop('host', 'id');
+        http.flush();
+    });
+
+    it('should restart a container', function () {
+        http.expect('POST',
+            '/api/v0/host/d/containers/id/restart').respond('');
+
+        containerService.restart('host', 'id');
+        http.flush();
+    });
+
+    it('should kill a container', function () {
+        http.expect('POST',
+            '/api/v0/host/d/containers/id/kill').respond('');
+
+        containerService.kill('host', 'id');
+        http.flush();
+    });
+
+    it('should pause a container', function () {
+        http.expect('POST',
+            '/api/v0/host/d/containers/id/pause').respond('');
+
+        containerService.pause('host', 'id');
+        http.flush();
+    });
+
+    it('should unpause a container', function () {
+        http.expect('POST',
+            '/api/v0/host/d/containers/id/unpause').respond('');
+
+        containerService.unpause('host', 'id');
+        http.flush();
+    });
+
+    it('should wait on a container', function () {
+        http.expect('POST',
+            '/api/v0/host/d/containers/id/wait').respond('');
+
+        containerService.wait('host', 'id');
+        http.flush();
+    });
+
+    it('should remove a container', function () {
+        http.expect('DELETE',
+            '/api/v0/host/d/containers/id').respond('');
+
+        containerService.remove('host', 'id');
+        http.flush();
+    });
+
 });
 
