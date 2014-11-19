8feb242c3c0ccfce02e8c8a7c41e7cc3e0fe8f5c
Zach Taylor <ztaylor234@gmail.com>
2014-11-19 13:25:11 -0600

    Started on the instance/vm/provider service

--- app/js/instances/instanceController.js
+++ app/js/instances/instanceController.js
@@ -5,7 +5,7 @@
 function instanceController($scope, instanceService) {
     'use strict';
 
-    $scope.message = instanceService.getInstances();
+    $scope.instances = instanceService.getInstances();
 }
 
 instanceController.$inject = ['$scope', 'instanceService'];
--- app/js/instances/instanceService.js
+++ app/js/instances/instanceService.js
@@ -3,15 +3,34 @@
  * Requests and manages information related to instance discovery.
  */
 function instanceService(Restangular) {
+  'use strict';
 
     var baseInstances = Restangular.all('instances');
 
-    function getInstances() {
-        return 'hello from instanceService';
+    function instanceBase(provider) {
+      return Restangular
+        .one('plugins').one(provider);
+    }
+
+    function getInstances(provider, query, headers) {
+        return instanceBase
+            .one('vms').get(query, headers);
+    }
+
+    function findInstances(provider, query, headers) {
+        return instanceBase
+            .one('vms').one('find').get(query, headers);
+    }
+
+    function authorizeInstance(provider, query, headers) {
+        return instanceBase
+            .one('authorize').get(query, headers);
     }
 
     return {
-        'getInstances': getInstances
+        'getInstances': getInstances,
+        'findInstances': findInstances,
+        'authorizeInstance': authorizeInstance
     };
 }
 
