05ce86ba19820e25bd9bdad5b0498be5697b5e39
Nick Miller <ngmiller@iastate.edu>
2014-11-27 17:35:19 -0600

    add logout functionality to navbar

--- app/js/auth/authService.js
+++ app/js/auth/authService.js
@@ -34,27 +34,32 @@ function authService($rootScope, $q, Restangular, authEvents) {
     function login(auth) {
         var deferred = $q.defer();
 
-        Restangular.all('login').post(auth).then(
-            function (response) {
-                if (response === 'false') {
-                    deferred.reject('invalid email or password');
-                }
-                else {
-                    var user = {
-                        email: auth.Email
-                    };
+        if (isLoggedIn()) {
+            deferred.resolve($rootScope.user);
+        }
+        else {
+            Restangular.all('login').post(auth).then(
+                function (response) {
+                    if (response === 'false') {
+                        deferred.reject('invalid email or password');
+                    }
+                    else {
+                        var user = {
+                            email: auth.Email
+                        };
 
-                    $rootScope.user = user;
-                    deferred.resolve(user);
+                        $rootScope.user = user;
+                        deferred.resolve(user);
 
-                    // fire event
-                    $rootScope.$broadcast(authEvents.login);
+                        // fire event
+                        $rootScope.$broadcast(authEvents.login);
+                    }
                 }
-            }
-            // NOTE
-            // error callback will not execute on failed login
-            // as server returns 200 OK in either case
-        );
+                // NOTE
+                // error callback will not execute on failed login
+                // as server returns 200 OK in either case
+            );
+        }
 
         return deferred.promise;
     }
@@ -62,18 +67,36 @@ function authService($rootScope, $q, Restangular, authEvents) {
     /*
      * logout()
      * Invalidate current session.
+     *
+     * @return promise: (success, error)
+     *      - both callbacks may accept the response object returned from
+     *        the initial /logout call
      */
     function logout() {
-        Restangular.one('logout').get().then(
-            // success
-            function (response) {
+        var deferred = $q.defer();
 
-            },
-            // error
-            function (response) {
+        if (!isLoggedIn()) {
+            deferred.resolve('logged out');
+        }
+        else {
+            Restangular.one('logout').get().then(
+                // success
+                function (response) {
+                    // invalidate user object
+                    $rootScope.user = null;
+                    deferred.resolve(response);
 
-            }
-        );
+                    // fire event
+                    $rootScope.$broadcast(authEvents.logout);
+                },
+                // error
+                function (response) {
+                    deferred.reject(response);
+                }
+            );
+        }
+
+        return deferred.promise;
     }
 
     return {
--- app/js/nav/navController.js
+++ app/js/nav/navController.js
@@ -2,16 +2,33 @@
  * navController
  * Manage navigation bar status and information.
  */
-function navController($scope, authEvents, authService) {
+function navController($scope, $location, authEvents, authService) {
     // initialize nav
     $scope.loggedIn = authService.isLoggedIn();
 
     // update nav
     $scope.$on(authEvents.login, function (event, data) {
-        $scope.loggedIn = authService.isLoggedIn();
+        $scope.loggedIn = true;
         $scope.user = authService.getUser();
     });
+
+    $scope.logout = function () {
+        authService.logout().then(
+            // success
+            function (response) {
+                $scope.loggedIn = false;
+                $scope.user = authService.getUser();
+                // redirect
+                $location.path('/login');
+            },
+            // error
+            function (response) {
+                // TODO
+                // wire in alertService
+            }
+        );
+    };
 }
 
-navController.$inject = ['$scope', 'authEvents', 'authService'];
+navController.$inject = ['$scope', '$location', 'authEvents', 'authService'];
 module.exports = navController;
