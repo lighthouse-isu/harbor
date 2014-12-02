9b16607591d4330ee524712a47f2ffce40571b1f
Zach Taylor <ztaylor234@gmail.com>
2014-12-02 16:55:41 -0600

    Add actions for starting, stopping, and restarting the containers

--- app/js/instances/instanceDetailController.js
+++ app/js/instances/instanceDetailController.js
@@ -31,6 +31,18 @@ function instanceDetailController($scope, $routeParams, instanceService, contain
             console.log(response);
         }
     );
+
+    $scope.start = function (id) {
+        containerService.start(host, id);
+    }
+
+    $scope.stop = function (id) {
+        containerService.stop(host, id);
+    }
+
+    $scope.restart = function (id) {
+        containerService.restart(host, id);
+    }
 }
 
 instanceDetailController.$inject = ['$scope', '$routeParams', 'instanceService', 'containerService'];
